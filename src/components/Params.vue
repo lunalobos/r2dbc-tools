2f31b21b500794f17c7cad56e29a55d83ee7ed6a
lunalobos <lunalobosmiguel@gmail.com>
2024-11-25 07:56:41 -0300

    Update Params.vue

--- src/components/Params.vue
+++ src/components/Params.vue
@@ -21,7 +21,7 @@
             <div class="w-1/6">
                 <button type="button" class="bg-red-800 hover:bg-red-900 text-white 
                 font-bold py-2 px-4 rounded-2xl delay-300 transition-all align-middle " @click="deteleProperty(inputProperty.internalId)">
-                    <svg class="w-6 h-6 text-gray-800 dark:text-white" aria-hidden="true"
+                    <svg class="w-6 h-6 text-white" aria-hidden="true"
                         xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
                         <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
                             d="M5 7h14m-9 3v8m4-8v8M10 3h4a1 1 0 0 1 1 1v3H9V4a1 1 0 0 1 1-1ZM6 7h12v13a1 1 0 0 1-1 1H7a1 1 0 0 1-1-1V7Z" />
@@ -53,7 +53,7 @@
                 <button type="button" class="bg-green-700 hover:bg-green-800 text-white 
                 font-bold py-2 px-4 rounded-2xl delay-300 transition-all"
                     @click="inputProperties.push({ name: '', id: false, type: '' })">
-                    <svg class="w-6 h-6 text-gray-800 dark:text-white" aria-hidden="true"
+                    <svg class="w-6 h-6 text-white" aria-hidden="true"
                         xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
                         <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
                             d="M12 7.757v8.486M7.757 12h8.486M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
